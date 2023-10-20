5673cf84188f02d0e8fe949c231f3bb34322b29b
Hermes J. Pappas <hjp@hjp.gr>
2023-10-20 12:54:06 +0300

    Set look for button and search/select bar

--- pages/data/plan-designer.vue
+++ pages/data/plan-designer.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="w-full h-full">
         <Heading :title="$t('data.designer')" />
-        <USelectMenu
-            v-model="selected"
-            searchable
-            searchable-placeholder="Search a dataset..."
-            class="w-full mt-8 lg:w-60"
-            placeholder="Select a dataset"
-            :options="selections"
-        />
+        <div class="flex gap-4 items-center mt-8">
+            <USelectMenu
+                v-model="selected"
+                icon="i-heroicons-magnifying-glass-20-solid"
+                searchable
+                searchable-placeholder="Search for a dataset..."
+                class="w-full lg:w-72 relative"
+                placeholder="Select / search for a dataset"
+                :options="selections"
+            />
+            <UButton label="File Browser" />
+        </div>
     </div>
 </template>
 
