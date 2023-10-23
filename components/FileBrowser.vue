caad177aae821e90aec307a39b60e2db44eebfcc
Hermes J. Pappas <hjp@hjp.gr>
2023-10-23 08:42:31 +0300

    Fix wrapping

--- components/FileBrowser.vue
+++ components/FileBrowser.vue
@@ -1,17 +1,19 @@
 <template>
     <UCard>
-        <div class="w-full flex flex-row flex-wrap gap-y-4 gap-x-2 overflow-y-auto h-96 items-start">
-            <div
-                v-for="file in files"
-                :key="file"
-                :class="[
-                    'w-24 flex flex-col gap-2 justify-center items-center cursor-pointer p-2 rounded-xl border-2 border-gray-500 border-opacity-0',
-                    file === innerSelected ? 'bg-gray-100 border-opacity-50' : 'border-opacity-0',
-                ]"
-                @click="$emit('selectionChanged', file)"
-            >
-                <UIcon name="i-fa6-regular-file-lines" class="w-16 h-16 text-gray-500" />
-                <p class="text-xs text-gray-600">{{ file }}</p>
+        <div class="w-full h-96">
+            <div class="w-full flex flex-row flex-wrap gap-y-5 gap-x-2 overflow-y-auto max-h-96 items-start">
+                <div
+                    v-for="file in files"
+                    :key="file"
+                    :class="[
+                        'w-24 flex flex-col gap-2 justify-center items-center cursor-pointer p-2 rounded-xl border-2 border-gray-500 border-opacity-0',
+                        file === innerSelected ? 'bg-gray-100 border-opacity-50' : 'border-opacity-0',
+                    ]"
+                    @click="$emit('selectionChanged', file)"
+                >
+                    <UIcon name="i-fa6-regular-file-lines" class="w-16 h-16 text-gray-500" />
+                    <p class="text-xs text-gray-600">{{ file }}</p>
+                </div>
             </div>
         </div>
     </UCard>
