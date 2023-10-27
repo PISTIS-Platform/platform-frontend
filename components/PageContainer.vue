6116bb758506d5dfc0ba3169e8a79447c9322e53
PanayiotisG <wolfmanp91@gmail.com>
2023-10-27 15:17:51 +0300

    change in page container component

--- components/PageContainer.vue
+++ components/PageContainer.vue
@@ -1,17 +1,5 @@
-<script lang="ts" setup>
-const props = defineProps({
-    title: {
-        type: String,
-        required: false,
-        default: '',
-    },
-});
-</script>
-
 <template>
     <div class="flex flex-col py-6 justify-start items-start px-8 flex-1 max-w-6xl mx-auto w-full">
-        <Heading v-if="props.title" :title="$t('admin.services.title')" />
-
         <slot />
     </div>
 </template>
