9aeff3d739e466f460ef75ce2d8bd4ccda80169f
statosEft <s.eftaxiadis@gmail.com>
2023-11-08 18:42:52 +0200

    fix: layout updates, fix types

--- components/FileBrowser.vue
+++ components/FileBrowser.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const props = defineProps({
+defineProps({
     files: {
         type: Array<string>,
         required: true,
@@ -18,11 +18,11 @@ const emit = defineEmits(['update:model-value']);
         <div class="w-full h-96">
             <div class="w-full flex flex-row flex-wrap gap-y-5 gap-x-2 lg:gap-x-7 max-h-96 items-start">
                 <div
-                    v-for="file in props.files"
+                    v-for="file in files"
                     :key="file"
                     :class="[
                         'w-24 flex flex-col gap-2 justify-center items-center cursor-pointer p-2 rounded-xl border-2 border-primary-500 border-opacity-0',
-                        file === props.modelValue ? 'bg-primary-50 border-opacity-100' : 'border-opacity-0',
+                        file === modelValue ? 'bg-primary-50 border-opacity-100' : 'border-opacity-0',
                     ]"
                     @click="emit('update:model-value', file)"
                 >
--- components/plan-designer/DatasetSelector.vue
+++ components/plan-designer/DatasetSelector.vue
@@ -5,7 +5,7 @@ import dummyData from '../../pages/data/dummy-data';
 
 const { t } = useI18n();
 
-const props = defineProps({
+defineProps({
     selected: {
         type: String,
         required: true,
@@ -21,7 +21,7 @@ const emit = defineEmits(['reset', 'update:selected', 'update:complete-or-query'
 //data for selecting specific dataset
 
 const selections = Object.keys(dummyData);
-const displayTable = ref(false);
+const displayTable = ref<boolean>(false);
 
 const switchDatasetOpen = ref<boolean>(false);
 
@@ -29,7 +29,7 @@ const filteredSelections = computed(() =>
     outerQuery.value ? selections.filter((selection: string) => selection.includes(outerQuery.value)) : selections,
 );
 
-const outerQuery = ref('');
+const outerQuery = ref<string>('');
 
 const dataSetSelections = computed(() => [
     {
@@ -43,7 +43,7 @@ const dataSetSelections = computed(() => [
 ]);
 
 function toggleTable() {
-    return (displayTable.value = !displayTable.value);
+    displayTable.value = !displayTable.value;
 }
 </script>
 
@@ -55,100 +55,108 @@ function toggleTable() {
                 :info="$t('data.designer.datasetSelectionInfo')"
             />
         </template>
-
-        <span
-            v-if="props.selected"
-            class="flex gap-2 items-center text-primary cursor-pointer w-60"
-            @click="switchDatasetOpen = true"
-        >
-            <UIcon name="i-heroicons-arrow-left-20-solid" class="h-5 w-5" />
-            {{ $t('data.designer.selectDifferent') }}
-        </span>
-
-        <UModal v-model="switchDatasetOpen">
-            <UCard class="flex flex-col justify-center items-center text-center text-gray-700 h-40">
-                <p class="font-bold text-xl">{{ $t('data.designer.areYouSure') }}</p>
-                <p class="text-gray-400 mt-6">{{ $t('data.designer.willReset') }}</p>
-                <div class="flex gap-8 w-full justify-center mt-6">
-                    <UButton color="white" class="w-20 flex justify-center" @click="switchDatasetOpen = false">{{
-                        $t('cancel')
-                    }}</UButton>
-                    <UButton class="w-20 flex justify-center" @click="emit('reset'), (switchDatasetOpen = false)">{{
-                        $t('yes')
-                    }}</UButton>
-                </div>
-            </UCard>
-        </UModal>
-        <span v-if="!props.selected" class="flex gap-1">
-            <USelectMenu
-                :model-value="props.selected"
-                icon="i-heroicons-magnifying-glass-20-solid"
-                :searchable="
-                    (query: string) => {
-                        outerQuery = query;
-                        return selections.filter((selection: string) => selection.includes(query));
-                    }
-                "
-                :searchable-placeholder="$t('data.designer.searchDataset')"
-                class="w-11/12"
-                :placeholder="$t('data.designer.selectSearchDataset')"
-                :options="selections"
-                @update:model-value="(value: string) => emit('update:selected', value)"
-            />
-
-            <UButton class="" @click="toggleTable"> Browse datasets </UButton>
-        </span>
-        <span v-show="displayTable">
-            <Transition
-                enter-active-class="duration-300 ease-out"
-                enter-from-class="transform opacity-0"
-                enter-to-class="opacity-100"
-            >
-                <FileBrowser
-                    v-if="!props.selected"
-                    :model-value="props.selected"
-                    class="mt-6"
-                    :files="filteredSelections"
+        <div class="space-y-5">
+            <div v-if="!selected" class="flex">
+                <USelectMenu
+                    :model-value="selected"
+                    icon="i-heroicons-magnifying-glass-20-solid"
+                    :searchable="
+                        (query: string) => {
+                            outerQuery = query;
+                            return selections.filter((selection: string) => selection.includes(query));
+                        }
+                    "
+                    :searchable-placeholder="$t('data.designer.searchDataset')"
+                    :placeholder="$t('data.designer.selectSearchDataset')"
+                    :options="selections"
+                    size="md"
+                    class="w-full"
                     @update:model-value="(value: string) => emit('update:selected', value)"
                 />
-            </Transition>
 
-            <Transition
-                enter-active-class="duration-300 ease-out"
-                enter-from-class="transform opacity-0"
-                enter-to-class="opacity-100"
-                leave-active-class="duration-300 ease-in"
-                leave-from-class="opacity-100"
-                leave-to-class="transform opacity-0"
-            >
-                <div v-if="props.selected" class="flex gap-4 mt-8 w-full">
-                    <div class="flex flex-col items-start justify-start gap-4 whitespace-nowrap">
-                        <p>{{ $t('data.designer.assetTitle') }}:</p>
-                        <p>{{ $t('data.designer.assetDescription') }}:</p>
-                    </div>
-                    <div class="flex flex-col items-start justify-start gap-4">
-                        <p class="font-bold">{{ props.selected }}</p>
-                        <p>{{ dummyData[props.selected].description }}</p>
-                    </div>
-                </div>
-            </Transition>
-
-            <Transition
-                enter-active-class="duration-300 ease-out"
-                enter-from-class="transform opacity-0"
-                enter-to-class="opacity-100"
-                leave-active-class="duration-300 ease-in"
-                leave-from-class="opacity-100"
-                leave-to-class="transform opacity-0"
-            >
-                <SelectionCards
-                    v-if="props.selected"
-                    :model-value="completeOrQuery"
-                    class="mt-8"
-                    :selections="dataSetSelections"
-                    @update:model-value="(value: string) => emit('update:complete-or-query', value)"
+                <UButton
+                    size="md"
+                    block
+                    class="w-36 ml-2"
+                    :label="displayTable ? 'Hide datasets' : 'Browse datasets'"
+                    @click="toggleTable"
                 />
-            </Transition>
-        </span>
+            </div>
+            <div
+                v-else
+                class="flex gap-2 items-center text-primary cursor-pointer w-60"
+                @click="switchDatasetOpen = true"
+            >
+                <UIcon name="i-heroicons-arrow-left-20-solid" class="h-5 w-5" />
+                <span>{{ $t('data.designer.selectDifferent') }}</span>
+            </div>
+
+            <div>
+                <div v-if="!selected && displayTable">
+                    <Transition
+                        enter-active-class="duration-300 ease-out"
+                        enter-from-class="transform opacity-0"
+                        enter-to-class="opacity-100"
+                    >
+                        <FileBrowser
+                            :model-value="selected"
+                            class=""
+                            :files="filteredSelections"
+                            @update:model-value="(value: string) => emit('update:selected', value)"
+                        />
+                    </Transition>
+                </div>
+                <div v-if="selected" class="space-y-5">
+                    <Transition
+                        enter-active-class="duration-300 ease-out"
+                        enter-from-class="transform opacity-0"
+                        enter-to-class="opacity-100"
+                        leave-active-class="duration-300 ease-in"
+                        leave-from-class="opacity-100"
+                        leave-to-class="transform opacity-0"
+                    >
+                        <div class="flex gap-4 w-full">
+                            <div class="flex flex-col items-start justify-start gap-4 whitespace-nowrap">
+                                <p>{{ $t('data.designer.assetTitle') }}:</p>
+                                <p>{{ $t('data.designer.assetDescription') }}:</p>
+                            </div>
+                            <div class="flex flex-col items-start justify-start gap-4">
+                                <p class="font-bold">{{ selected }}</p>
+                                <p>{{ dummyData[selected].description }}</p>
+                            </div>
+                        </div>
+                    </Transition>
+                    <Transition
+                        enter-active-class="duration-300 ease-out"
+                        enter-from-class="transform opacity-0"
+                        enter-to-class="opacity-100"
+                        leave-active-class="duration-300 ease-in"
+                        leave-from-class="opacity-100"
+                        leave-to-class="transform opacity-0"
+                    >
+                        <SelectionCards
+                            :model-value="completeOrQuery"
+                            class="gap-4"
+                            :selections="dataSetSelections"
+                            @update:model-value="(value: string) => emit('update:complete-or-query', value)"
+                        />
+                    </Transition>
+                </div>
+            </div>
+        </div>
     </UCard>
+    <UModal v-model="switchDatasetOpen">
+        <UCard class="flex flex-col justify-center items-center text-center text-gray-700 h-40">
+            <p class="font-bold text-xl">{{ $t('data.designer.areYouSure') }}</p>
+            <p class="text-gray-400 mt-6">{{ $t('data.designer.willReset') }}</p>
+            <div class="flex gap-8 w-full justify-center mt-6">
+                <UButton color="white" class="w-20 flex justify-center" @click="switchDatasetOpen = false">{{
+                    $t('cancel')
+                }}</UButton>
+                <UButton class="w-20 flex justify-center" @click="emit('reset'), (switchDatasetOpen = false)">{{
+                    $t('yes')
+                }}</UButton>
+            </div>
+        </UCard>
+    </UModal>
 </template>
