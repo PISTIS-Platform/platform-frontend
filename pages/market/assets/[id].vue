595f56b526b636c379307d1c709b3e38fa15f673
Hermes J. Pappas <hjp@hjp.gr>
2024-04-05 14:53:57 +0300

    Create info bar

--- pages/market/assets/[id].vue
+++ pages/market/assets/[id].vue
@@ -7,7 +7,7 @@ import { BasicAsset } from '~/interfaces/market-insights';
 
 const route = useRoute();
 
-const assetID = route.params.id;
+const _assetID = route.params.id;
 
 //TODO: Get asset info from API call
 const assets: BasicAsset[] = [
@@ -36,11 +36,54 @@ const assets: BasicAsset[] = [
         data: [13, 56, 34, 20, 34, 45, 60],
     },
 ];
+
+//TODO: Get with API call using asset ID
+const assetInfo = {
+    title: 'Asset Title Here',
+    seller: 'Seller Name Here',
+    views: 500000,
+    monetization_plan: 'Subscription',
+    data_quality_index: 5,
+    sector: 'Aviation',
+};
 </script>
 
 <template>
     <PageContainer>
         <RollingAssetsBar :assets="assets" />
-        {{ assetID }}
+        <ChartContainer :title="assetInfo.title" class="mt-6">
+            <div class="flex w-full justify-between items-center text-gray-500">
+                <div class="flex gap-2 items-center">
+                    <UIcon name="i-fa6-regular-user" class="h-6 w-6 text-primary-500" />
+                    <span class="text-sm"
+                        >Seller: <span class="font-bold">{{ assetInfo.seller }}</span></span
+                    >
+                </div>
+                <div class="flex gap-2 items-center">
+                    <UIcon name="i-fa6-regular-eye" class="h-6 w-6 text-primary-500" />
+                    <span class="text-sm"
+                        >Views: <span class="font-bold">{{ $n(assetInfo.views) }}</span></span
+                    >
+                </div>
+                <div class="flex gap-2 items-center">
+                    <UIcon name="i-fa6-regular-money-bill-1" class="h-6 w-6 text-primary-500" />
+                    <span class="text-sm"
+                        >Monetisation Plan: <span class="font-bold">{{ assetInfo.monetization_plan }}</span></span
+                    >
+                </div>
+                <div class="flex gap-2 items-center">
+                    <UIcon name="i-fa6-regular-rectangle-list" class="h-6 w-6 text-primary-500" />
+                    <span class="text-sm"
+                        >Sector: <span class="font-bold">{{ assetInfo.sector }}</span></span
+                    >
+                </div>
+                <div class="flex gap-2 items-center">
+                    <UIcon name="i-fa6-regular-file-lines" class="h-6 w-6 text-primary-500" />
+                    <span class="text-sm"
+                        >Data Quality Index: <span class="font-bold">{{ assetInfo.data_quality_index }}</span></span
+                    >
+                </div>
+            </div>
+        </ChartContainer>
     </PageContainer>
 </template>
