200f2f9974c0dc3f09370a1e4902e51090ea17cb
Hermes J. Pappas <hjp@hjp.gr>
2024-04-05 12:50:06 +0300

    Attribute not working on Nuxt UI component, revert to full object v-model

--- pages/market/assets.vue
+++ pages/market/assets.vue
@@ -4,28 +4,25 @@ import type Selection from '~/interfaces/selection';
 
 const { t } = useI18n();
 
-// import {
-//     ArcElement,
-//     BarElement,
-//     CategoryScale,
-//     Chart as ChartJS,
-//     Legend,
-//     LinearScale,
-//     LineElement,
-//     PointElement,
-//     Title,
-//     Tooltip,
-// } from 'chart.js';
-// import { Bar, Doughnut, Line } from 'vue-chartjs';
+import {
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+} from 'chart.js';
+import { Line } from 'vue-chartjs';
 
-// import { data, lineData, options, pieData } from './chart-data';
-
-// ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement, ArcElement);
+ChartJS.register(CategoryScale, LinearScale, Title, Tooltip, Legend, PointElement, LineElement);
 
 const selectedInterval = ref('D');
 
 const triggerIntervalChangeSelection = (value: string) => {
     selectedInterval.value = value;
+    //TODO: Do something with the change here
 };
 
 const assets: BasicAsset[] = [
@@ -67,7 +64,36 @@ const sectors: Selection[] = [
     },
 ];
 
-const selectedSector = ref<Selection>(sectors[0]);
+const selectedSector = ref(sectors[0]);
+
+const _triggerSectorChangeSelection = (value: Selection) => {
+    selectedSector.value = value;
+    //TODO: Do something with the change here
+    //selectedSector.value.value -> actual value
+};
+
+const lineData = {
+    labels: ['January', 'February', 'March', 'April'],
+    datasets: [
+        {
+            label: 'Earnings',
+            borderColor: '#7BA3A2',
+            backgroundColor: '#7BA3A2',
+            data: [20, 12, 11, 39],
+        },
+        {
+            label: 'Costs',
+            borderColor: '#4B6C6C',
+            backgroundColor: '#4B6C6C',
+            data: [11, 20, 12, 39],
+        },
+    ],
+};
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+};
 </script>
 
 <template>
@@ -78,7 +104,7 @@ const selectedSector = ref<Selection>(sectors[0]);
                 <RollingAsset :asset-info="asset" />
             </UCard>
         </div>
-        <ChartContainer :title="$t('market.assets.timeline')" class="mt-6">
+        <ChartContainer :title="$t('market.assets.timeline')" class="mt-6 h-96">
             <template #right-header>
                 <div class="flex items-center gap-6">
                     <TimeframeSelector
@@ -86,15 +112,12 @@ const selectedSector = ref<Selection>(sectors[0]);
                         is-interval
                         @update:model-value="triggerIntervalChangeSelection"
                     />
-                    <USelectMenu
-                        v-model="selectedSector"
-                        size="md"
-                        :options="sectors"
-                        value-attribute="value"
-                        option-attribute="label"
-                    />
+                    <USelectMenu v-model="selectedSector" size="md" :options="sectors"> </USelectMenu>
                 </div>
             </template>
+            <div class="h-72 w-full">
+                <Line class="w-full h-full" :data="lineData" :options="options" />
+            </div>
         </ChartContainer>
     </PageContainer>
 </template>
