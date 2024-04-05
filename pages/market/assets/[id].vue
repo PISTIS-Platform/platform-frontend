b2f3819bec1dbcc05105eb4ba93bbe09fcdd4dc1
Hermes J. Pappas <hjp@hjp.gr>
2024-04-05 14:28:14 +0300

    Get asset id from route param

---
+++ pages/market/assets/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute } from 'nuxt/app';
+
+// const { t } = useI18n();
+
+const route = useRoute();
+
+const assetID = route.params.id;
+</script>
+
+<template>
+    {{ assetID }}
+</template>
