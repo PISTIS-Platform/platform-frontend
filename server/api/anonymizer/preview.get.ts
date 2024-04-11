2f7f1e013acf841ca8e12df7209400bc4e13a059
Juice Dev Machine <cam.skip.dev352@gmail.com>
2024-04-11 21:30:34 +0100

    Added authentication

--- server/api/anonymizer/preview.get.ts
+++ server/api/anonymizer/preview.get.ts
@@ -1,7 +1,13 @@
+import { getToken } from '#auth';
+
 const { anonymizerApiUrl } = useRuntimeConfig();
 
-export default defineEventHandler(async () => {
-    const response = await fetch(`${anonymizerApiUrl}/api/dataset/preview`);
+export default defineEventHandler(async (event) => {
+    const token = (await getToken({ event })) || { access_token: 'null' };
+
+    const response = await fetch(`${anonymizerApiUrl}/api/dataset/preview`, {
+        headers: { Authorization: `Bearer ${token!.access_token}` },
+    });
     const json = await response.json();
 
     return json;
--- server/api/anonymizer/solution.post.ts
+++ server/api/anonymizer/solution.post.ts
@@ -1,12 +1,15 @@
 const { anonymizerApiUrl } = useRuntimeConfig();
+import { getToken } from '#auth';
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
+    const token = (await getToken({ event })) || { access_token: 'null' };
 
     const response = await fetch(`${anonymizerApiUrl}/api/k-anon/solution`, {
         method: 'POST',
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
+            Authorization: `Bearer ${token!.access_token}`,
         },
         body: JSON.stringify(body),
     });
