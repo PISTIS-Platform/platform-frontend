32950e71e540d96a508c553f05d851b90d11c8f2
Hermes J. Pappas <hjp@hjp.gr>
2024-07-05 08:53:53 +0300

    Change look of navigation steps

--- pages/data/publish-data.vue
+++ pages/data/publish-data.vue
@@ -236,10 +236,10 @@ const limitFrequencySelections = computed(() => [
 ]);
 
 const steps = [
-    { id: 'Step 1', name: t('data.designer.nav.selectDataset'), href: 'select', status: 'current' },
-    { id: 'Step 2', name: t('data.designer.nav.monetizationPlanner'), href: 'planner', status: 'upcoming' },
-    { id: 'Step 3', name: t('data.designer.nav.accessPoliciesEditor'), href: 'editor', status: 'upcoming' },
-    { id: 'Step 4', name: t('data.designer.nav.preview'), href: 'preview', status: 'upcoming' },
+    { id: '1', name: t('data.designer.nav.selectDataset'), href: 'select', status: 'current' },
+    { id: '2', name: t('data.designer.nav.monetizationPlanner'), href: 'planner', status: 'upcoming' },
+    { id: '3', name: t('data.designer.nav.accessPoliciesEditor'), href: 'editor', status: 'upcoming' },
+    { id: '4', name: t('data.designer.nav.preview'), href: 'preview', status: 'upcoming' },
 ];
 
 const selectedPage = ref('select'); //other value is 'preview'
@@ -291,36 +291,66 @@ const handleDatasetSelection = (dataset: { id: string | number; title: string; d
 </script>
 
 <template>
-    <nav aria-label="Progress" class="w-full mb-8">
-        <ol role="list" class="space-y-4 md:flex md:space-x-8 md:space-y-0">
+    <nav aria-label="Progress">
+        <ol role="list" class="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0 mb-8">
             <li
-                v-for="step in steps"
+                v-for="(step, stepIdx) in steps"
                 :key="step.name"
-                class="md:flex-1 cursor-pointer"
+                class="relative md:flex md:flex-1 cursor-pointer"
                 @click="handleStepSelect(step.href)"
             >
-                <a
-                    v-if="step.status === 'complete'"
-                    class="group flex flex-col border-l-4 border-indigo-600 py-2 pl-4 hover:border-indigo-800 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
-                >
-                    <span class="text-sm font-medium text-indigo-600 group-hover:text-indigo-800">{{ step.id }}</span>
-                    <span class="text-sm font-medium">{{ step.name }}</span>
+                <a v-if="step.status === 'complete'" class="group flex w-full items-center">
+                    <span class="flex items-center px-6 py-4 text-sm font-medium">
+                        <span
+                            class="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-indigo-600 text-white group-hover:bg-indigo-800"
+                        >
+                            <UIcon name="i-fa6-regular-circle-check" class="text-white h-5 w-5" />
+                        </span>
+                        <span class="ml-4 text-sm font-medium text-gray-900">{{ step.name }}</span>
+                    </span>
                 </a>
                 <a
                     v-else-if="step.status === 'current'"
-                    class="flex flex-col border-l-4 border-indigo-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+                    class="flex items-center px-6 py-4 text-sm font-medium"
                     aria-current="step"
                 >
-                    <span class="text-sm font-medium text-indigo-600">{{ step.id }}</span>
-                    <span class="text-sm font-medium">{{ step.name }}</span>
+                    <span
+                        class="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-indigo-600"
+                    >
+                        <span class="text-indigo-600">{{ step.id }}</span>
+                    </span>
+                    <span class="ml-4 text-sm font-medium text-indigo-600">{{ step.name }}</span>
                 </a>
-                <a
-                    v-else
-                    class="group flex flex-col border-l-4 border-gray-200 py-2 pl-4 hover:border-gray-300 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
-                >
-                    <span class="text-sm font-medium text-gray-500 group-hover:text-gray-700">{{ step.id }}</span>
-                    <span class="text-sm font-medium">{{ step.name }}</span>
+                <a v-else class="group flex items-center">
+                    <span class="flex items-center px-6 py-4 text-sm font-medium">
+                        <span
+                            class="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300 group-hover:border-gray-400"
+                        >
+                            <span class="text-gray-500 group-hover:text-gray-900">{{ step.id }}</span>
+                        </span>
+                        <span class="ml-4 text-sm font-medium text-gray-500 group-hover:text-gray-900">{{
+                            step.name
+                        }}</span>
+                    </span>
                 </a>
+                <template v-if="stepIdx !== steps.length - 1">
+                    <!-- Arrow separator for lg screens and up -->
+                    <div class="absolute right-0 top-0 hidden h-full w-5 md:block" aria-hidden="true">
+                        <svg
+                            class="h-full w-full text-gray-300"
+                            viewBox="0 0 22 80"
+                            fill="none"
+                            preserveAspectRatio="none"
+                        >
+                            <path
+                                d="M0 -2L20 40L0 82"
+                                vector-effect="non-scaling-stroke"
+                                stroke="currentcolor"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                    </div>
+                </template>
             </li>
         </ol>
     </nav>
