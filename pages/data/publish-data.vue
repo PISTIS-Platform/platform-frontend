5f8b4f528a0abd5f1fb8272fa54ee8cdc62a9642
Hermes J. Pappas <hjp@hjp.gr>
2024-07-10 11:42:06 +0300

    Remove dom printout

--- pages/data/publish-data.vue
+++ pages/data/publish-data.vue
@@ -146,8 +146,6 @@ const handleDatasetSelection = (dataset: { id: string | number; title: string; d
 </script>
 
 <template>
-    {{ isAssetOfferingDetailsValid }}
-    {{ isMonetizationValid }}
     <nav aria-label="Progress">
         <ol role="list" class="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0 mb-8">
             <li
