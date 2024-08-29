d06a70fcf8d10079335222ecf36115825d0274a4
Hermes J. Pappas <hjp@hjp.gr>
2024-08-29 10:59:12 +0300

    Remove dummy notifications

--- store/messages.ts
+++ store/messages.ts
@@ -4,45 +4,7 @@ import type { Message } from '~/interfaces/message';
 
 export const useMessagesStore = defineStore('MessagesStore', {
     state: (): { messages: Message[] } => ({
-        messages: [
-            {
-                id: '1',
-                userId: '2',
-                organizationId: '3',
-                message: 'Your notification text goes here.',
-                isHidden: false,
-                createdAt: new Date(),
-                readAt: null,
-            },
-            {
-                id: '2',
-                userId: '2',
-                organizationId: '3',
-                message:
-                    'Sit ad dolor sint. Qui laboris Lorem elit velit. Incididunt nulla nisi ipsum et aliquip nulla fugiat ullamco. Quis sit elit exercitation mollit elit ut sint. Et id dolore velit cillum. Commodo fugiat proident elit irure exercitation deserunt sint. Nulla velit consectetur sit mollit ex.',
-                isHidden: false,
-                createdAt: new Date(),
-                readAt: null,
-            },
-            {
-                id: '3',
-                userId: '2',
-                organizationId: '3',
-                message: 'Your notification text goes here.',
-                isHidden: true,
-                createdAt: new Date(),
-                readAt: null,
-            },
-            {
-                id: '4',
-                userId: '2',
-                organizationId: '3',
-                message: 'Your notification text goes here.',
-                isHidden: false,
-                createdAt: new Date(),
-                readAt: new Date(),
-            },
-        ],
+        messages: [],
     }),
     getters: {
         getMessages(): Message[] {
