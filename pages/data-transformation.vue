98f52fc22175266b26f15d06f5168713d33c2a1a
Sergio Salmeron <sergio.salmeron@eviden.com>
2024-09-04 10:58:10 +0000

    added data-transformation uppre navigation bar

---
+++ pages/data-transformation.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const routes = ref([
+    { name: 'Data transformation', to: '/data-transformation' },
+    { name: 'Data transformation catalog', to: '/data-transformation/catalog' },
+    //{ name: 'data.wallet.title', to: '/data' },
+    //{ name: 'data.log', to: '/data/log' },
+    //{ name: 'data.designer.title', to: '/data/publish-data' },
+]);
+</script>
+
+<template>
+    <ShellLayout :navigation="routes">
+        <div class="w-full h-full flex flex-col justify-start">
+            <NuxtPage />
+        </div>
+    </ShellLayout>
+</template>
