0c0dc108b56e20adc3cf1199cd027cfc6daa809b
Hermes J. Pappas <hjp@hjp.gr>
2024-09-23 13:01:59 +0300

    Fix

--- composables/monetization.ts
+++ composables/monetization.ts
@@ -6,65 +6,63 @@ export const useMonetizationSchema = () => {
     const isWorldwide = ref(false);
     const isPerpetual = ref(false);
 
-    const oneOffSaleSchema = z
-        .object({
-            type: z.literal('one-off'),
-            price: z.coerce.number({ invalid_type_error: t('val.validNumber') }).refine(
+    const oneOffSaleSchema = z.object({
+        type: z.literal('one-off'),
+        price: z.coerce.number({ invalid_type_error: t('val.validNumber') }).refine(
+            (val) => {
+                return isFree.value ? val === 0 : val > 0;
+            },
+            {
+                message: isFree.value ? '' : t('data.designer.priceHigherThanZero'),
+            },
+        ),
+        license: z.string().min(1, t('val.required')),
+        extraTerms: z.string().min(20, t('val.atLeastNumberChars', { count: 20 })),
+        contractTerms: z.string().min(1, t('val.required')),
+        limitNumber: z.coerce
+            .number({ invalid_type_error: t('val.validNumber') })
+            .gte(0, t('val.zeroOrPositive'))
+            .refine(
                 (val) => {
-                    return isFree.value ? val === 0 : val > 0;
+                    return val > 0;
                 },
                 {
-                    message: isFree.value ? '' : t('data.designer.priceHigherThanZero'),
+                    message: t('val.zeroOrPositive'),
                 },
             ),
-            license: z.string().min(1, t('val.required')),
-            extraTerms: z.string().min(20, t('val.atLeastNumberChars', { count: 20 })),
-            contractTerms: z.string().min(1, t('val.required')),
-            limitNumber: z.coerce
-                .number({ invalid_type_error: t('val.validNumber') })
-                .gte(0, t('val.zeroOrPositive'))
-                .refine(
-                    (val) => {
-                        return val > 0;
-                    },
-                    {
-                        message: t('val.zeroOrPositive'),
-                    },
-                ),
-            limitFrequency: z.string().min(1, t('val.required')),
-        })
-        .required();
+        limitFrequency: z.string().min(1, t('val.required')),
+        isExclusive: z.boolean().optional(),
+    });
 
-    const subscriptionSchema = z
-        .object({
-            type: z.literal('subscription'),
-            subscriptionFrequency: z.string().min(1, t('val.required')),
-            // price: z.coerce.number({ invalid_type_error: t('val.validNumber') }).gt(0, t('val.zeroOrPositive')),
-            price: z.coerce.number({ invalid_type_error: t('val.validNumber') }).refine(
+    const subscriptionSchema = z.object({
+        type: z.literal('subscription'),
+        subscriptionFrequency: z.string().min(1, t('val.required')),
+        // price: z.coerce.number({ invalid_type_error: t('val.validNumber') }).gt(0, t('val.zeroOrPositive')),
+        price: z.coerce.number({ invalid_type_error: t('val.validNumber') }).refine(
+            (val) => {
+                return isFree.value ? val === 0 : val > 0;
+            },
+            {
+                message: isFree.value ? '' : t('data.designer.priceHigherThanZero'),
+            },
+        ),
+        license: z.string().min(1, t('val.required')),
+        extraTerms: z.string().min(20, t('val.atLeastNumberChars', { count: 20 })),
+        contractTerms: z.string().min(1, t('val.required')),
+        limitNumber: z.coerce
+            .number({ invalid_type_error: t('val.validNumber') })
+            .gte(0, t('val.zeroOrPositive'))
+            .refine(
                 (val) => {
-                    return isFree.value ? val === 0 : val > 0;
+                    return val > 0;
                 },
                 {
-                    message: isFree.value ? '' : t('data.designer.priceHigherThanZero'),
+                    message: t('val.zeroOrPositive'),
                 },
             ),
-            license: z.string().min(1, t('val.required')),
-            extraTerms: z.string().min(20, t('val.atLeastNumberChars', { count: 20 })),
-            contractTerms: z.string().min(1, t('val.required')),
-            limitNumber: z.coerce
-                .number({ invalid_type_error: t('val.validNumber') })
-                .gte(0, t('val.zeroOrPositive'))
-                .refine(
-                    (val) => {
-                        return val > 0;
-                    },
-                    {
-                        message: t('val.zeroOrPositive'),
-                    },
-                ),
-            limitFrequency: z.string().min(1, t('val.required')),
-        })
-        .required();
+        limitFrequency: z.string().min(1, t('val.required')),
+        isExclusive: z.boolean().optional(),
+    });
 
     const monetizationSchema = z.union([oneOffSaleSchema, subscriptionSchema]);
 
