fd82e2b45680869929a330f6de9a3392e7183553
a752932 <135056717+a752932@users.noreply.github.com>
2024-10-21 11:31:53 +0200

    feat: add data transformation page (#39)
    * Initial commit for data-transformation
    
    * added catalog
    
    * added prueba, that works sending a file in the POST request body
    
    * working version as index.vue
    
    * output format selection
    
    * result viz adapted to the content format
    
    * added data-transformation uppre navigation bar
    
    * comments removed
    
    * updates to data transformation
    
    * hotfix: minor fix in route path in catalog.vue
    
    * hotfix: minor fix in route path in index.vue
    
    * fix: fixing minor issues from the api call from the data transformation vue
    
    * fix: typo in env variable
    
    ---------
    
    Co-authored-by: Sergio Salmeron <sergio.salmeron@eviden.com>

---
+++ pages/data-transformation/index.vue
@@ -0,0 +1,152 @@
+<script lang="ts" setup>
+const jsonContent = ref('');
+const fileUpload = ref<File | null>(null);
+const responseContent = ref('');
+const outputFormat = ref('application/json'); // Default value
+
+const handleFileChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files.length > 0) {
+        fileUpload.value = target.files[0];
+    }
+};
+
+const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append('transformation_definition', jsonContent.value);
+
+    if (fileUpload.value) {
+        formData.append('file', fileUpload.value);
+    }
+
+    try {
+        const contentType = outputFormat.value;
+
+        const response = await $fetch.raw('/api/data-transformation/transformation', {
+            method: 'POST',
+            body: formData,
+            query: {
+                Accept: outputFormat.value,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        // constcontentType = response.headers.get('Content-Type');
+        if (contentType === 'application/json') {
+            const data = response._data;
+            responseContent.value = JSON.stringify(data, null, 2);
+        } else if (contentType?.includes('text/plain')) {
+            const data = response._data;
+            responseContent.value = data as string;
+        } else if (contentType?.includes('text/xml')) {
+            const data = response._data;
+            responseContent.value = data as string;
+        } else if (contentType?.includes('application/vnd.ms-excel')) {
+            const blob = new Blob([response._data as string]);
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'response.xlsx';
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+            responseContent.value = 'Excel file downloaded.';
+        } else {
+            responseContent.value = 'Unsupported response type';
+        }
+
+        //console.log('Success:', responseContent.value);
+    } catch (error) {
+        responseContent.value = `Error: ${error.message}`;
+        console.error('Error:', error);
+    }
+};
+</script>
+
+<template>
+    <div class="container mx-auto p-6 bg-white rounded-xl shadow-md space-y-6">
+        <div class="form-container">
+            <form @submit.prevent="handleSubmit">
+                <div class="p-4 bg-gray-100 rounded-lg shadow">
+                    <label for="jsonContent" class="block text-sm font-medium text-gray-700"
+                        >Transformation Definition:</label
+                    >
+                    <textarea
+                        id="jsonContent"
+                        v-model="jsonContent"
+                        rows="10"
+                        cols="50"
+                        class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    ></textarea>
+                </div>
+
+                <div class="p-4 bg-gray-100 rounded-lg shadow">
+                    <label for="fileUpload" class="block text-sm font-medium text-gray-700">Upload Dataset:</label>
+                    <input
+                        id="fileUpload"
+                        type="file"
+                        class="mt-1 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
+                        @change="handleFileChange"
+                    />
+                </div>
+
+                <div class="p-4 bg-gray-100 rounded-lg shadow">
+                    <label for="outputFormat" class="block text-sm font-medium text-gray-700">Output format:</label>
+                    <select
+                        id="outputFormat"
+                        v-model="outputFormat"
+                        class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    >
+                        <option value="text/plain">CSV</option>
+                        <option value="application/vnd.ms-excel">Excel</option>
+                        <option value="text/xml">XML</option>
+                        <option value="application/json">JSON</option>
+                    </select>
+                </div>
+
+                <div class="p-4 bg-gray-100 rounded-lg shadow text-right">
+                    <button
+                        type="submit"
+                        class="px-4 py-2 bg-indigo-600 text-white rounded-md shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        <div class="response-container">
+            <div class="p-4 bg-gray-100 rounded-lg shadow">
+                <label for="responseContent" class="block text-sm font-medium text-gray-700">Response:</label>
+                <textarea
+                    id="responseContent"
+                    v-model="responseContent"
+                    rows="10"
+                    cols="50"
+                    class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    readonly
+                ></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+}
+
+.form-container {
+    flex: 1;
+}
+
+.response-container {
+    flex: 1;
+}
+</style>
---
+++ server/routes/api/data-transformation/transformation.post.ts
@@ -0,0 +1,18 @@
+import { getToken } from '#auth';
+
+const { dataTransfApiUrl } = useRuntimeConfig();
+
+export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+    const body = await readBody(event);
+    const token = await getToken({ event });
+
+    return event.$fetch(`${dataTransfApiUrl}/transform/`, {
+        method: 'POST',
+        body,
+        headers: {
+            Authorization: `Bearer ${token?.access_token}`,
+            Accept: `${query.Accept}`,
+        },
+    });
+});
