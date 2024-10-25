7d809894ec4bb6451646ffe3c8f50c9a92c48d61
Marios Phinikettos <finikm@gmail.com>
2024-10-25 18:31:37 +0300

    fix: add missing trailing slash

--- server/routes/api/datasets/publish-data.post.ts
+++ server/routes/api/datasets/publish-data.post.ts
@@ -8,7 +8,8 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     const token = await getToken({ event });
 
-    return $fetch(`${factoryUrl}/srv/asset-description-bundler/api/adb`, {
+    // DO NOT REMOVE THE TRAILING SLASH FROM THE URL!
+    return $fetch(`${factoryUrl}/srv/asset-description-bundler/api/adb/`, {
         method: 'POST',
         body,
         headers: {
