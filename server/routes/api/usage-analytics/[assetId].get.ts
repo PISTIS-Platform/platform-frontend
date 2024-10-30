1fbeea8f56a1dab6e23ce8cff416e86b7ae885d0
PanayiotisG <59557373+wolfmanpg@users.noreply.github.com>
2024-10-30 11:32:19 +0200

    fix: change endpoint in nitro (#61)

--- server/routes/api/usage-analytics/[assetId].get.ts
+++ server/routes/api/usage-analytics/[assetId].get.ts
@@ -8,9 +8,12 @@ export default defineEventHandler(async (event) => {
     const assetId = getRouterParam(event, 'assetId');
     const token = await getToken({ event });
 
-    return await $fetch(`${cloudUrl}/srv/intention-analytics/api/questionnaire/${assetId}/active-questionnaire/`, {
-        headers: {
-            Authorization: `Bearer ${token?.access_token}`,
+    return await $fetch(
+        `${cloudUrl}/srv/intention-analytics/api/questionnaire/${assetId}/active-questionnaire/verified-buyers`,
+        {
+            headers: {
+                Authorization: `Bearer ${token?.access_token}`,
+            },
         },
-    });
+    );
 });
