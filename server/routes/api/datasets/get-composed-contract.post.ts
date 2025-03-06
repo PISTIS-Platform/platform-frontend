8847b61de6718f7d1a7b1baed8eac57aceab4105
Hermes J. Pappas <hjp@hjp.gr>
2025-03-06 13:24:36 +0200

    Return directly

--- server/routes/api/datasets/get-composed-contract.post.ts
+++ server/routes/api/datasets/get-composed-contract.post.ts
@@ -8,15 +8,11 @@ export default defineEventHandler(async (event) => {
     const token = await getToken({ event });
     const body = await readBody(event);
 
-    const result = await $fetch(`${factoryUrl}/srv/sctc/api/sctc/compose`, {
+    return $fetch(`${factoryUrl}/srv/sctc/api/sctc/compose`, {
         method: 'POST',
         body,
         headers: {
             Authorization: `Bearer ${token?.access_token}`,
         },
     });
-
-    console.log({ result });
-
-    return result;
 });
