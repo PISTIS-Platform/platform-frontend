e3fc0a801d6fd42dce92c681868eb453077e12a3
Hermes J. Pappas <hjp@hjp.gr>
2025-06-24 11:39:42 +0300

    Add authenticated check to FE auth middleware and don't show page content unless user is authenticated

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,11 +1,12 @@
-export default defineNuxtRouteMiddleware(async (_to) => {
-    const { data: session, signOut } = useAuth();
+export default defineNuxtRouteMiddleware(async (to) => {
+    const { data: session, signOut, status } = useAuth();
     const runtimeConfig = useRuntimeConfig();
 
     if (session.value && session.value?.orgId !== runtimeConfig.public.orgId) {
-        // await navigateTo('https://pistis-market.eu/', {
-        //     external: true,
-        // });
-        signOut();
+        signOut({ callbackUrl: '/' });
+    }
+
+    if (to.path !== '/' && (!session.value || status.value !== 'authenticated')) {
+        signOut({ callbackUrl: '/' });
     }
 });
