b0a9f538ba0dc5d52d68b5b370db256abb9cb7ad
Hermes J. Pappas <hjp@hjp.gr>
2025-06-24 14:44:12 +0300

    Add error card

---
+++ components/error-card.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+    errorMsg: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <UAlert
+        class="p-6 border border-red-200"
+        icon="i-heroicons-exclamation-circle"
+        :title="errorMsg"
+        color="red"
+        variant="soft"
+    />
+    <slot> </slot>
+</template>
--- server/routes/api/monitoring/component-status.get.ts
+++ server/routes/api/monitoring/component-status.get.ts
@@ -9,51 +9,77 @@ import ComponentStatusData from '~/interfaces/component-status-data';
 export default defineEventHandler(async (event) => {
     const token = await getToken({ event });
 
-    const results = await $fetch(`${config.public.cloudUrl}/srv/factories-registry/api/factories/services-mapping`, {
+    const services = await $fetch(`${config.public.cloudUrl}/srv/factories-registry/api/factories/services-mapping`, {
         method: 'GET',
         headers: {
             Authorization: `Bearer ${token?.access_token}`,
         },
     });
 
-    console.log({ results });
+    const componentStatuses: ComponentStatusData[] = [];
 
-    const componentStatuses: ComponentStatusData[] = [
-        {
-            title: 'Data Exchange Governance',
-            active: true,
-        },
-        {
-            title: 'Data Explorer',
-            active: false,
-        },
-        {
-            title: 'Monetary Transactions Facility',
-            active: true,
-        },
-        {
-            title: 'Asset Offering Designer',
-            active: true,
-        },
-        {
-            title: 'Monetization XAI Engine',
-            active: false,
-        },
-        {
-            title: 'Models Repository',
-            active: true,
-        },
-        {
-            title: 'System Services',
-            active: false,
-        },
-        {
-            title: 'Identity Manager',
-            active: false,
-        },
-    ];
+    Object.keys(services).forEach(async (key: string) => {
+        let active = false;
+
+        try {
+            const result = await $fetch(`${config.public.cloudUrl}${services[key]}/api/health`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token?.access_token}`,
+                },
+            });
+
+            if (result.status === 'ok') {
+                active = true;
+            }
+        } catch {
+            //TODO: Do something more advanced with errors here
+        }
+
+        componentStatuses.push({
+            title: key,
+            active,
+        });
+    });
+
+    // const componentStatuses: ComponentStatusData[] = [
+    //     {
+    //         title: 'Data Exchange Governance',
+    //         active: true,
+    //     },
+    //     {
+    //         title: 'Data Explorer',
+    //         active: false,
+    //     },
+    //     {
+    //         title: 'Monetary Transactions Facility',
+    //         active: true,
+    //     },
+    //     {
+    //         title: 'Asset Offering Designer',
+    //         active: true,
+    //     },
+    //     {
+    //         title: 'Monetization XAI Engine',
+    //         active: false,
+    //     },
+    //     {
+    //         title: 'Models Repository',
+    //         active: true,
+    //     },
+    //     {
+    //         title: 'System Services',
+    //         active: false,
+    //     },
+    //     {
+    //         title: 'Identity Manager',
+    //         active: false,
+    //     },
+    // ];
 
     const sortByTitle = R.sortBy(R.compose(R.toLower, R.prop('title')));
+
+    console.log(componentStatuses);
 
     return sortByTitle(componentStatuses);
 });
