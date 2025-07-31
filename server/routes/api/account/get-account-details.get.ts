43528c7ca32d636cf43e93ebfc91dff7b17f9dab
Hermes J. Pappas <hjp@hjp.gr>
2025-07-31 10:57:55 +0300

    Rewrite Nitro call

--- server/routes/api/account/get-account-details.get.ts
+++ server/routes/api/account/get-account-details.get.ts
@@ -2,29 +2,21 @@ import { getToken } from '#auth';
 
 export default defineEventHandler(async (event) => {
     const token = await getToken({ event });
-    let user;
-    let org;
 
-    if (token) {
-        user = {
+    if (!token) return;
+
+    return {
+        user: {
             name: token.name,
             email: token.email,
             roles: token.pistis.user.role,
-        };
-
-        org = {
+        },
+        org: {
             name: token.orgName,
             country: token.pistis.group.country,
             size: token.pistis.group.size,
             domain: token.pistis.group.domain,
             type: token.pistis.group.type,
-        };
-    } else {
-        return null;
-    }
-
-    return {
-        user,
-        org,
+        },
     };
 });
