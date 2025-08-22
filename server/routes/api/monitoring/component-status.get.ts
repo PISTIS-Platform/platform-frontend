f2479108418e82c2443f56290491f7a3b565dd75
Giannis Giannopoulos <89904471+Ggiannopoulos@users.noreply.github.com>
2025-08-22 09:36:00 +0300

    fix: monitoring (#126)

--- server/routes/api/monitoring/component-status.get.ts
+++ server/routes/api/monitoring/component-status.get.ts
@@ -9,37 +9,95 @@ import ComponentStatusData from '~/interfaces/component-status-data';
 export default defineEventHandler(async (event) => {
     const token = await getToken({ event });
 
-    const services = await $fetch<Record<string, string>>(
-        `${config.public.cloudUrl}/srv/factories-registry/api/factories/services-mapping`,
-        {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token?.access_token}`,
-            },
-        },
-    );
+    const newServices = {
+        // 'analytics-engine': '',
+        // eslint-disable-next-line prettier/prettier
+        Anonymizer: [],
+        'Asset Description Bundler': ['/srv/asset-description-bundler/api/health/'],
+        'On/Off platform contract inspector': [],
+        'Data Check-in': [],
+        'Data Factory Connector': ['/srv/data-connector/api/health'],
+        'Identity Wallet': [],
+        'Data Quality Assessment': ['/srv/data-quality-assessment/api/health/'],
+        'Data Transformation': [],
+        'FAIR Data Valuation': [],
+        'Distributed Query': [],
+        'DLT FIAT Wallet': [],
+        'Encryption/Decryption': [],
+        'Factory Data Storage': ['/srv/factory-data-storage/api/health'],
+        'GDPR Checker': ['/srv/gdpr-checker/ready'],
+        'Insights Generator': [],
+        // 'job-configurator': '',
+        // 'ledger-validator': '',
+        'Lineage Tracker': [],
+        'Factory Data Catalog': ['/srv/catalog'],
+        'Smart contract template composer': ['/srv/sctc/api/health'],
+        'Smart Contract Execution Engine': ['/srv/smart-contract-execution-engine/ready'],
+    };
 
-    const componentStatusPromises = Object.keys(services).map(async (key: string) => {
-        let active = false;
-        const url = `${config.public.factoryUrl}${services[key]}/api/health`.replace('//', '/');
-        try {
-            const result = await $fetch(url, {
+    const componentStatusPromises = Object.entries(newServices).map(async ([key, value]: [string, string[]]) => {
+        let active = 'false';
+        const url = `${config.public.factoryUrl}${value}`;
+        if (value.length === 0) {
+            return {
+                title: key,
+                active: 'N/A',
+            };
+        }
+        if (key === 'Factory Data Catalog') {
+            const repo = await $fetch(`${config.public.factoryUrl}/srv/repo/health`, {
                 method: 'GET',
                 headers: {
                     Authorization: `Bearer ${token?.access_token}`,
                 },
+                timeout: 5000,
+            });
+            const search = await $fetch(`${config.public.factoryUrl}/srv/search/health`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token?.access_token}`,
+                },
+                timeout: 5000,
             });
 
-            if (result?.status === 'ok') {
-                active = true;
+            const mqa = await $fetch(`${config.public.factoryUrl}/srv/mqa/health`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token?.access_token}`,
+                },
+                timeout: 5000,
+            });
+            if (repo?.status === 'UP' && search?.status === 'UP' && mqa?.status === 'UP') {
+                active = 'true';
             }
-        } catch (error) {
-            console.error(`Error fetching health for ${key}:`, error);
-        } finally {
             return {
                 title: key,
                 active,
             };
+        } else {
+            try {
+                const result = await $fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${token?.access_token}`,
+                    },
+                    timeout: 5000,
+                });
+                if (result === 'Service is up and running') {
+                    active = 'true';
+                } else if (result?.success) {
+                    active = 'true';
+                } else if (result?.status.toLowerCase() === 'ok') {
+                    active = 'true';
+                }
+            } catch (error) {
+                console.error(`Error fetching health for ${key}:`, error);
+            } finally {
+                return {
+                    title: key,
+                    active,
+                };
+            }
         }
     });
 
