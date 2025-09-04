f8312c7088aee6bcac49bc7f8011195666368eb4
Hermes J. Pappas <hjp@hjp.gr>
2025-09-04 15:12:30 +0300

    Fix facets

---
+++ server/routes/api/datasets/get-all-on-marketplace.get.ts
@@ -0,0 +1,29 @@
+import { getToken } from '#auth';
+
+const {
+    public: { cloudUrl },
+} = useRuntimeConfig();
+
+export default defineEventHandler(async (event) => {
+    const token = await getToken({ event });
+    // const tokenData = jwtDecode(token?.access_token);
+
+    const facets = {
+        monetizationType: ['one-off', 'subscription'],
+        publisher: ['ACME'],
+    };
+
+    const results: any = await $fetch(
+        `${cloudUrl}/srv/search/search?filters=dataset&facets=${encodeURIComponent(JSON.stringify(facets))}`,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token?.access_token}`,
+            },
+        },
+    );
+
+    console.log({ results });
+
+    return results.result.results;
+});
