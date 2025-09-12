11617800b23dc313d576f58871288f48801e9fee
Hermes J. Pappas <hjp@hjp.gr>
2025-09-12 11:53:51 +0300

    Revert widening

--- layouts/default.vue
+++ layouts/default.vue
@@ -92,7 +92,7 @@ function isActive(item: any) {
 <template>
     <div class="h-full flex flex-col">
         <Disclosure v-slot="{ open }" as="nav" class="bg-primary-700 sticky top-0 z-50">
-            <div class="mx-auto px-8 max-w-[1600px]">
+            <div class="mx-auto max-w-7xl">
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
