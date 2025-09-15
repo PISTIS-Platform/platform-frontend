c645f8e1afff5d9f69668b0f837f71cbcd01e604
Hermes J. Pappas <hjp@hjp.gr>
2025-09-15 10:59:54 +0300

    Fix highlight logic and pages / containers width (#150)
    * Fix highlight logic and pages / containers width
    
    * Revert widening
    
    * Comment out

--- components/catalog/search-page/SearchBar.vue
+++ components/catalog/search-page/SearchBar.vue
@@ -7,12 +7,12 @@ const searchInput = defineModel<string>();
 </script>
 
 <template>
-    <div class="mt-[5px] flex-none px-6 pb-10">
+    <div class="mt-[5px] w-full px-6 pb-10">
         <section name="top" class="flex flex-col">
             <form class="flex gap-3" @submit.prevent="searchAction">
                 <SearchInput
                     v-model="searchInput"
-                    class="w-full max-w-[50rem] ring-gray-200 ring-1 shadow rounded-md"
+                    class="w-full ring-gray-200 ring-1 shadow rounded-md"
                     :placeholder="$t('searchBar.searchBarPlaceholder')"
                     :select-options="[]"
                 />
