e7a75c24963871332ba38981028f85635c531ea1
Hermes J. Pappas <hjp@hjp.gr>
2025-09-15 15:17:27 +0300

    Move to FE with answer script

--- pages/usage-analytics/[assetId]/questionnaire.vue
+++ pages/usage-analytics/[assetId]/questionnaire.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { v4 as uuidv4 } from 'uuid';
+
+dayjs.extend(customParseFormat);
+
 import type {
     Question,
     QuestionAnswer,
@@ -6,11 +12,11 @@ import type {
     QuestionOption,
     SelectedOption,
 } from '~/interfaces/usage-analytics';
+import { QuestionType } from '~/interfaces/usage-analytics';
 
 const { t } = useI18n();
 const { showSuccessMessage, showErrorMessage } = useAlertMessage();
 const route = useRoute();
-const router = useRouter();
 
 const answers = ref<QuestionAnswer[]>([]);
 const errorMsg = ref('');
@@ -21,42 +27,30 @@ const {
     pending: loadingQuestionnaire,
     error,
 } = await useAsyncData<Questionnaire>(`fetchQuestionnaire-asset-${route.params.assetId}`, () =>
-    $fetch(`/api/usage-analytics/${route.params.assetId}`),
+    $fetch(`/api/questionnaire/${route.params.assetId}`),
 );
 
 if (error.value || !questionnaire.value) {
     const serverError = error.value as Record<string, any>;
 
-    if (serverError?.statusCode === 400 || serverError?.statusCode === 404) {
-        errorMsg.value = serverError?.data?.data?.message || t('usageAnalytics.errorInRetrievingQuestionnaire');
-        //TODO:: add navigation to go back to previous page (?)
-    } else {
-        showErrorMessage(t('usageAnalytics.errorInRetrievingQuestionnaire'));
-    }
+    errorMsg.value = serverError?.data?.data?.message || t('usage-analytics.errorWhileRetrievingActiveVersion');
 } else {
-    let answerId = 0;
-
     answers.value =
         questionnaire.value?.questions?.map((question: Question) => {
             let availableOptions: SelectedOption[] = [];
 
             if (question?.options) {
-                let id = 0;
                 availableOptions = question?.options.map((option: QuestionOption) => {
-                    id++;
-
                     return {
-                        id: id.toString(),
+                        id: uuidv4(),
                         value: option?.text || '',
                         isSelected: false,
                     };
                 });
             }
 
-            answerId++;
-
             return {
-                id: answerId.toString(),
+                id: uuidv4(),
                 text: '',
                 questionType: question.type,
                 availableOptions,
@@ -75,14 +69,86 @@ const isSubmitDisabled = computed(() => {
     return false;
 });
 
+function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const _submitRandomValues = async () => {
+    let date = dayjs('18/06/2025', 'DD/MM/YYYY').set('hour', 0).set('minute', 0).set('second', 0);
+    for (let d = 0; d < 30; d++) {
+        date = date.add(1, 'day');
+        for (let i = 0; i < 34; i++) {
+            const body: {
+                userId: string;
+                assetId: string;
+                createdAt: string;
+                responses: {
+                    questionId: string;
+                    questionTitle: string;
+                    text?: string;
+                    options?: string[];
+                }[];
+            } = {
+                userId: uuidv4(),
+                assetId: route.params.assetId as string,
+                createdAt: date.format(),
+                responses: answers.value.map((answer) => {
+                    const chosenOptions: string[] = [];
+                    const from = 0;
+                    const to = answer.availableOptions?.length || 0 - 1;
+
+                    if (answer.questionType === QuestionType.CHECKBOX) {
+                        const times = getRandomInt(from + 1, to + 1);
+                        for (let j = 0; j < times; j++) {
+                            const randomOption = answer.availableOptions?.[getRandomInt(from, to)]?.value || '';
+                            while (!chosenOptions.includes(randomOption) && randomOption) {
+                                chosenOptions.push(randomOption);
+                            }
+                        }
+                    } else if (answer.questionType === QuestionType.RADIO) {
+                        chosenOptions.push(answer.availableOptions?.[getRandomInt(from, to)]?.value || '');
+                    }
+                    return {
+                        questionId: answer.question?.id ?? '',
+                        questionTitle: answer.question?.title ?? '',
+                        text: answer?.text,
+                        options: chosenOptions,
+                    };
+                }),
+            };
+
+            try {
+                await $fetch(`/api/questionnaire/submit-answers`, {
+                    query: {
+                        id: questionnaire.value?.id,
+                        version: questionnaire.value?.version,
+                    },
+                    method: 'post',
+                    body,
+                });
+
+                showSuccessMessage(t('usage-analytics.answersSubmitted'));
+            } catch (error) {
+                showErrorMessage(t('usage-analytics.errorInSubmitAnswers'));
+
+                submitPending.value = false;
+            }
+        }
+    }
+};
+
+// submitRandomValues();
+
 const saveAnswers = async () => {
-    // if even at least 1 answer has validation errors -> do not proceed
-    if (answers.value.some((a: QuestionAnswer) => !a.isValid)) {
-        showErrorMessage(t('usageAnalytics.checkInputs'));
+    if (!questionnaire.value) {
         return;
     }
 
-    if (!questionnaire.value) {
+    // if even at least 1 answer has validation errors -> do not proceed
+    if (answers.value.some((a: QuestionAnswer) => !a.isValid)) {
+        showErrorMessage(t('usage-analytics.checkAnswers'));
         return;
     }
 
@@ -102,12 +168,12 @@ const saveAnswers = async () => {
             questionId: answer.question?.id ?? '',
             questionTitle: answer.question?.title ?? '',
             text: answer?.text,
-            options: answer.selectedOptions?.map((option: SelectedOption) => option.value),
+            options: answer?.selectedOptions,
         })),
     };
 
     try {
-        await $fetch(`/api/usage-analytics/submit-answers`, {
+        await $fetch(`/api/questionnaire/submit-answers`, {
             query: {
                 id: questionnaire.value?.id,
                 version: questionnaire.value?.version,
@@ -116,13 +182,14 @@ const saveAnswers = async () => {
             body,
         });
 
-        showSuccessMessage(t('usageAnalytics.answersSubmitted'));
-        await delay(3);
-        router.push({ name: 'home' });
-
-        //TODO:: add any navigation after success (?)
+        showSuccessMessage(t('usage-analytics.answersSubmitted'));
+        setTimeout(async () => {
+            await navigateTo(`https://pistis-market.eu/srv/catalog/datasets/${route.params.assetId}`, {
+                external: true,
+            });
+        }, 3000);
     } catch (error) {
-        showErrorMessage(t('usageAnalytics.errorInSubmitAnswers'));
+        showErrorMessage(t('usage-analytics.errorInSubmitAnswers'));
 
         submitPending.value = false;
     }
@@ -135,9 +202,7 @@ const saveAnswers = async () => {
             <UProgress animation="carousel" />
         </div>
         <div v-else-if="!loadingQuestionnaire && error && !questionnaire">
-            <UCard v-if="errorMsg" class="w-full">
-                <SubHeading :title="errorMsg" />
-            </UCard>
+            <ErrorCard :error-msg="errorMsg" />
         </div>
         <UCard v-else class="w-full">
             <template #header>
@@ -147,7 +212,7 @@ const saveAnswers = async () => {
             <!-- Questionnaire Answers -->
             <div class="flex flex-col space-y-8">
                 <div v-if="!questionnaire?.questions?.length" class="text-sm text-gray-500">
-                    {{ $t('usageAnalytics.noQuestionsWereFound') }}
+                    {{ $t('usage-analytics.noQuestionsWereFound') }}
                 </div>
                 <div v-for="answer in answers" v-else :key="answer.id">
                     <UCard :ui="{ base: 'overflow-visible' }">
@@ -163,7 +228,7 @@ const saveAnswers = async () => {
                         <Answer
                             :answer="answer"
                             @update:text="(value: string) => (answer.text = value)"
-                            @update:selected-options="(value: SelectedOption[]) => (answer.selectedOptions = value)"
+                            @update:selected-options="(value: string[]) => (answer.selectedOptions = value)"
                             @is-valid="(isValid: boolean) => (answer.isValid = isValid)"
                         />
                     </UCard>
@@ -172,7 +237,7 @@ const saveAnswers = async () => {
 
             <!-- Submit Buttons -->
             <div v-if="questionnaire?.questions?.length" class="flex gap-4 justify-between items-center mt-8">
-                <UTooltip :text="$t('usageAnalytics.submitAnswers')">
+                <UTooltip :text="$t('usage-analytics.submitAnswers')">
                     <UButton
                         size="lg"
                         :label="$t('submit')"
