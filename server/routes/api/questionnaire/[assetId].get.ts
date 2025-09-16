152fa7338623dd10faafc0d34ecbc2006de80269
Hermes J. Pappas <hjp@hjp.gr>
2025-09-16 08:25:10 +0300

    Add API call for fetching questionnaire

---
+++ server/routes/api/questionnaire/[assetId].get.ts
@@ -0,0 +1,19 @@
+import { getToken } from '#auth';
+
+const {
+    public: { cloudUrl },
+} = useRuntimeConfig();
+
+export default defineEventHandler(async (event) => {
+    const assetId = getRouterParam(event, 'assetId');
+    const token = await getToken({ event });
+
+    return await $fetch(
+        `${cloudUrl}/srv/intention-analytics/api/questionnaire/${assetId}/active-questionnaire/general-users`,
+        {
+            headers: {
+                Authorization: `Bearer ${token?.access_token}`,
+            },
+        },
+    );
+});
