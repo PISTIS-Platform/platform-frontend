3b4e47192480814b796a4a04d5a888d9c7d3bc23
Lisa Bachmann <lisa.bachmann@fokus.fraunhofer.de>
2025-09-30 10:18:03 +0200

    change endpoint url

--- components/catalog/dataset-details/data-lineage/stores/store.js
+++ components/catalog/dataset-details/data-lineage/stores/store.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+
 import mockData from './mock-lineage.json';
 
 const pistisMode = ref('');
@@ -120,7 +121,6 @@ export const useStore = defineStore('store', () => {
             // const response = mockCompare;
             // diffData.value = response;
 
-
             if (import.meta.env.DEV) {
                 console.log('Dataset comparison data received:', diffData.value);
             }
@@ -141,10 +141,8 @@ export const useStore = defineStore('store', () => {
         }
 
         try {
-
             const isCloud = pistisMode.value === 'cloud';
             const url = `${backendUrl.value}/srv/lineage-tracker/get_dataset_family_tree`;
-
 
             const response = await axios.get(url, {
                 params: {
