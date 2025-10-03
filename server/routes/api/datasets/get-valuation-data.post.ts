93e960ab0435fcaa504f3d5f44d66d47b495f24d
Hermes J. Pappas <hjp@hjp.gr>
2025-10-03 17:46:24 +0300

    Replace with new URL

--- server/routes/api/datasets/get-valuation-data.post.ts
+++ server/routes/api/datasets/get-valuation-data.post.ts
@@ -8,7 +8,8 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     const token = await getToken({ event });
 
-    return $fetch(`${factoryUrl}/srv/data-valuation-service/api/datavalue`, {
+    //DO NOT REMOVE THE TRAILING SLASH FROM THE URL
+    return $fetch(`${factoryUrl}/srv/data-valuation-service/api/datavalue/`, {
         method: 'POST',
         body,
         headers: {
