e0f3f4b23d82b2633d78d64603d4d1f88eda15fb
Hermes J. Pappas <hjp@hjp.gr>
2025-10-06 16:57:57 +0300

    Remove bearer token from /srv/search components after Torben's instructions

--- server/routes/api/datasets/is-on-marketplace.get.ts
+++ server/routes/api/datasets/is-on-marketplace.get.ts
@@ -1,20 +1,14 @@
-import { getToken } from '#auth';
-
 const {
     public: { cloudUrl },
 } = useRuntimeConfig();
 
 export default defineEventHandler(async (event) => {
-    const token = await getToken({ event });
     const query = getQuery(event);
 
     const result: Record<string, any> = await $fetch(
         `${cloudUrl}/srv/search/search?q=${query.id}&filters=dataset&fields=offer.original_id&includes=id,offer,monetization`,
         {
             method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token?.access_token}`,
-            },
         },
     );
 
