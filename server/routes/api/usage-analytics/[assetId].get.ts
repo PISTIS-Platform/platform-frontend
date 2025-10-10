9f34670ad10c977f49b69bf4c4d0c8bd02d215fc
Hermes J. Pappas <hjp@hjp.gr>
2025-10-10 14:47:13 +0300

    Connect to new BE endpoints, tested locally

--- server/routes/api/usage-analytics/[assetId].get.ts
+++ server/routes/api/usage-analytics/[assetId].get.ts
@@ -6,14 +6,26 @@ const {
 
 export default defineEventHandler(async (event) => {
     const assetId = getRouterParam(event, 'assetId');
+    const query = getQuery(event);
     const token = await getToken({ event });
 
-    return await $fetch(
-        `${cloudUrl}/srv/intention-analytics/api/questionnaire/${assetId}/active-questionnaire/verified-buyers`,
-        {
-            headers: {
-                Authorization: `Bearer ${token?.access_token}`,
+    if (query.forVerifiedBuyers) {
+        return await $fetch(
+            `${cloudUrl}/srv/intention-analytics/api/questionnaire/${assetId}/active-questionnaire/verified-buyers`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token?.access_token}`,
+                },
             },
-        },
-    );
+        );
+    } else {
+        return await $fetch(
+            `${cloudUrl}/srv/intention-analytics/api/questionnaire/${assetId}/active-questionnaire/general-users`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token?.access_token}`,
+                },
+            },
+        );
+    }
 });
