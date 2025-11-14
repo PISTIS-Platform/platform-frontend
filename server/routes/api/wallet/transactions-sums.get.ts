bdef7ae49138e0023e6434c834fabeb092f3e782
Hermes J. Pappas <hjp@hjp.gr>
2025-11-14 08:44:18 +0200

    Change URL

--- server/routes/api/wallet/transactions-sums.get.ts
+++ server/routes/api/wallet/transactions-sums.get.ts
@@ -7,7 +7,7 @@ const {
 export default defineEventHandler(async (event) => {
     const token = await getToken({ event });
 
-    return $fetch(`${cloudUrl}/srv/transactions-auditor/api/transactions-auditor/transaction/sums-by-factory`, {
+    return $fetch(`${cloudUrl}/srv/transactions-auditor/api/transactions-auditor/transaction/summary`, {
         headers: {
             Authorization: `Bearer ${token?.access_token}`,
         },
