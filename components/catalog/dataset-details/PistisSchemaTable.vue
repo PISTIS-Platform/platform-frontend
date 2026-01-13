de85f61994553e13a3db952c7ed094a3baff601b
Lisa Bachmann <lisa.bachmann@fokus.fraunhofer.de>
2026-01-13 11:56:23 +0100

    fix pistisSchema table column width

--- components/catalog/dataset-details/PistisSchemaTable.vue
+++ components/catalog/dataset-details/PistisSchemaTable.vue
@@ -9,13 +9,13 @@ function shortDataType(dataType?: string): string {
 </script>
 
 <template>
-    <div class="w-full max-h-[400px] overflow-auto border">
-        <table class="min-w-full border-collapse">
+    <div class="w-full max-h-[400px] overflow-x-auto overflow-y-auto border">
+        <table class="border-collapse w-max min-w-full">
             <tr>
                 <th
                     v-for="column in sorted"
                     :key="column.column_id"
-                    class="border px-2 text-left text-sm text-gray-600"
+                    class="border px-3 py-2 text-left text-xs text-gray-600 min-w-[200px] whitespace-nowrap"
                 >
                     {{ column.column_name }}
                 </th>
@@ -24,12 +24,15 @@ function shortDataType(dataType?: string): string {
                 <td
                     v-for="col in sorted"
                     :key="col.column_id"
-                    class="border px-2 py-1 align-top text-gray-600 text-base"
+                    class="border px-3 py-2 align-top text-gray-600 text-sm min-w-[200px]"
                 >
                     <div>
-                        {{ col.property_name }} <span class="font-extralight">{{ shortDataType(col.data_type) }}</span>
+                        {{ col.property_name }}
+                        <span class="font-extralight">{{ shortDataType(col.data_type) }}</span>
                     </div>
-                    <div class="text-sm text-gray-500">{{ col.property_URI }}</div>
+                    <div class="text-xs text-gray-500 break-all">
+                        {{ col.property_URI }}
+                    </div>
                 </td>
             </tr>
         </table>
