4ca112ae606ebbd96ac4b7fb8d4bb685f6c979a4
lisamariebachmann <lisa.bachmann@fokus.fraunhofer.de>
2026-02-03 17:08:59 +0100

    Catalogue delete btn case2 (#354)
    * check marketplace for offers and check transactions
    
    * delete offer from marketplace

--- components/catalog/dataset-details/DeleteButton.vue
+++ components/catalog/dataset-details/DeleteButton.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import { useApiService } from '~/services/apiService';
+
+const config = useRuntimeConfig();
+
+const { getSCEEUrl } = useApiService();
 const { data: session } = useAuth();
 const token = ref(session.value?.token);
 
@@ -22,24 +27,60 @@ const props = defineProps({
 
 const router = useRouter();
 
-const {
-    data: isPublishedOnMarketplace,
-    status,
-    error,
-} = useAsyncData(() =>
-    $fetch(`/api/datasets/is-on-marketplace`, {
+const dist = ref([]);
+watchEffect(async () => {
+    dist.value = props.distributions;
+});
+
+const { data, status, error } = useAsyncData(() =>
+    $fetch('/api/datasets/is-on-marketplace', {
         query: { id: props.datasetId },
     }),
 );
 
-const dist = ref([]);
-watchEffect(() => {
-    dist.value = props.distributions;
+const isPublished = computed(() => data.value?.isPublished);
+const marketplaceResults = computed(() => data.value?.results);
+
+const offerIds = computed(() => {
+    if (!marketplaceResults.value) return [];
+    return marketplaceResults.value.map((offer) => offer.id);
 });
 
-const isPublished = computed(() => isPublishedOnMarketplace.value === true);
+const isOfferBought = async (offerId: string) => {
+    const sceeApi = getSCEEUrl(offerId);
+    try {
+        const response = await $fetch(sceeApi, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token.value}`,
+            },
+        });
+        return !!response;
+    } catch (err) {
+        console.error('SCEE ERROR:', err);
+    }
+};
 
-const canDelete = computed(() => status.value === 'success' && error.value === null && isPublished.value === false);
+const isBought = ref();
+watch(
+    offerIds,
+    async (ids) => {
+        if (!ids.length) {
+            isBought.value = false;
+            return;
+        }
+
+        const results = await Promise.all(ids.map((id) => isOfferBought(id)));
+        isBought.value = results.some(Boolean);
+    },
+    { immediate: true },
+);
+
+const canDelete = computed(() => status.value === 'success' && error.value === null && isBought.value === false);
+
+const openOfferDetailsPage = (offer) => {
+    window.open(`${config.public.factoryUrl}/marketplace/dataset-details/${offer}`);
+};
 
 const showConfirmationWindow = ref(false);
 
@@ -56,68 +97,101 @@ const confirmDelete = async () => {
 
     isDeleting.value = true;
 
-    try {
-        const response = await $fetch('/api/catalog/delete-dataset', {
-            method: 'DELETE',
-            query: { datasetId: props.datasetId },
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-            },
-        });
+    const deletedFromMarketplace = ref(false);
+    if (isPublished.value) {
+        try {
+            for (const offerId of offerIds.value) {
+                await $fetch('/api/catalog/delete-offer-from-marketplace', {
+                    method: 'DELETE',
+                    query: { offerId: offerId },
+                });
+            }
+            deletedFromMarketplace.value = true;
+        } catch (err) {
+            console.error('DELETE FROM MARKETPLACE ERROR:', err);
+            return;
+        }
+    }
 
-        deleteInfo.value = response.info;
-        deleteSuccess.value = true;
+    if (!isPublished.value || (isPublished.value && deletedFromMarketplace.value)) {
+        try {
+            const response = await $fetch('/api/catalog/delete-dataset', {
+                method: 'DELETE',
+                query: { datasetId: props.datasetId },
+                headers: {
+                    Authorization: `Bearer ${token.value}`,
+                },
+            });
 
-        setTimeout(() => {
-            showConfirmationWindow.value = false;
-            router.back();
-        }, 3500);
-    } catch (err) {
-        console.error('DELETE ERROR:', err);
-        deleteError.value = true;
-    } finally {
-        isDeleting.value = false;
+            deleteInfo.value = response.info;
+            deleteSuccess.value = true;
+
+            setTimeout(() => {
+                showConfirmationWindow.value = false;
+                router.back();
+            }, 3500);
+        } catch (err) {
+            console.error('DELETE ERROR:', err);
+            deleteError.value = true;
+        } finally {
+            isDeleting.value = false;
+        }
     }
 };
 </script>
 
 <template>
     <div v-if="props.catalog === 'my-data'">
-        <UButton
-            v-if="!isPublished && canDelete"
-            variant="solid"
-            color="red"
-            icon="i-heroicons-trash"
-            @click="openConfirmationWindow"
-        >
+        <UButton v-if="canDelete" variant="solid" color="red" icon="i-heroicons-trash" @click="openConfirmationWindow">
             Delete dataset
         </UButton>
-        <UTooltip v-else text="Dataset has been published and cannot be deleted.">
+        <UTooltip v-else text="Dataset has been acquired and cannot be deleted.">
             <UButton :disabled="true" color="red" icon="i-heroicons-trash"> Delete dataset </UButton>
         </UTooltip>
-        <UModal v-model="showConfirmationWindow" :ui="{ width: 'max-w-none w-[500px]' }">
+        <UModal v-model="showConfirmationWindow" :ui="{ width: 'max-w-none w-[650px]' }">
             <div v-if="!deleteSuccess">
-                <h2 class="text-lg font-semibold p-4">Delete dataset?</h2>
+                <h2 class="text-lg text-gray-700 font-semibold p-6">Delete dataset?</h2>
 
-                <p class="px-4">Are you sure you want to delete this dataset? This action cannot be undone.</p>
+                <p class="px-6 font-semibold text-gray-700">
+                    Are you sure you want to delete this dataset? This action cannot be undone.
+                </p>
                 <br />
-                <p class="px-4">The following distribution(s) will also be deleted:</p>
-                <ul class="px-10">
+                <p class="px-6 text-gray-700">The following distribution(s) will also be deleted:</p>
+                <ul class="px-12 text-gray-700">
                     <li v-for="distribution in dist" :key="distribution.id" class="list-disc">
-                        <span class="font-bold text-xs">({{ distribution.format.id }})</span>
+                        <span class="font-bold text-xs text-gray-700">({{ distribution.format.id }})</span>
                         {{ distribution.title?.en }}
                     </li>
                 </ul>
 
-                <div class="flex justify-end space-x-4 p-4">
+                <div v-if="isPublished">
+                    <p class="px-6 pt-10 font-semibold text-gray-700">
+                        This dataset has not yet been acquired but it has been published in the marketplace.
+                    </p>
+                    <br />
+                    <p class="px-6 text-gray-700">The following offer(s) will also be deleted:</p>
+                    <ul class="px-12">
+                        <li
+                            v-for="offer in offerIds"
+                            :key="offer.id"
+                            class="list-disc cursor-pointer underline text-gray-700"
+                            @click="openOfferDetailsPage(offer)"
+                        >
+                            {{ `${config.public.factoryUrl}/marketplace/dataset-details/${offer}` }}
+                            <UIcon name="i-heroicons-arrow-top-right-on-square" class="text-pistis-600"></UIcon>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="flex justify-end space-x-4 p-6">
                     <UButton variant="solid" color="gray" @click="showConfirmationWindow = false">Cancel</UButton>
                     <UButton variant="solid" color="red" @click="confirmDelete">Delete</UButton>
                 </div>
-                <div v-if="isDeleting" class="flex justify-end text-gray-600 px-4 pb-4">
+                <div v-if="isDeleting" class="flex justify-end text-gray-600 px-6 pb-6">
                     <div class="animate-spin rounded-full h-5 w-5 border-t-2"></div>
                     <p class="text-sm italic pl-2">Deleting dataset and distributions...</p>
                 </div>
-                <p v-if="deleteError" class="italic text-red-400 px-4 pb-4 text-sm text-right">
+                <p v-if="deleteError" class="italic text-red-400 px-4 pb-6 text-sm text-right">
                     Something went wrong, please try again.
                 </p>
             </div>
@@ -131,6 +205,12 @@ const confirmDelete = async () => {
                     <ul class="list-disc pl-5">
                         <li v-for="d in deleteInfo.distributions" :key="d.id">
                             <span class="font-medium">{{ d.title }}</span>
+                        </li>
+                    </ul>
+                    <p class="font-semibold my-1">Deleted from marketplace:</p>
+                    <ul class="list-disc pl-5">
+                        <li v-for="offer in offerIds" :key="offer.id">
+                            <span class="font-medium">{{ offer }}</span>
                         </li>
                     </ul>
                     <p class="font-semibold mb-1 mt-3">Dataset was also removed from:</p>
---
+++ server/routes/api/catalog/delete-offer-from-marketplace.delete.ts
@@ -0,0 +1,36 @@
+export default defineEventHandler(async (event) => {
+    const { offerId } = getQuery(event);
+    const config = useRuntimeConfig();
+
+    if (!offerId) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: 'offerId is required',
+        });
+    }
+
+    const baseUrl = config.public.cloudUrl;
+    const endpoint = `${baseUrl}/srv/repo/datasets/${offerId}`;
+    const apiKey = config.piveauHubRepoMarketplaceXApiKey;
+
+    const deleteResponse = await fetch(endpoint, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'x-api-key': apiKey,
+        },
+    });
+
+    if (!deleteResponse.ok) {
+        const msg = await deleteResponse.text();
+        throw createError({
+            statusCode: 500,
+            statusMessage: `Delete failed: ${msg}`,
+        });
+    }
+
+    return {
+        success: true,
+    };
+});
