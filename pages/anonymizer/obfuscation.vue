ae1d4c78f75f5ea9dd29aeec78823c39468ad434
josh-priestley87 <156321632+josh-priestley87@users.noreply.github.com>
2026-03-04 11:46:17 +0000

    Feat/anonymizer (#408)
    * Updated obfuscation.vue
    
    * Updated obfuscation.vue

--- pages/anonymizer/obfuscation.vue
+++ pages/anonymizer/obfuscation.vue
@@ -153,15 +153,18 @@ async function submitObfuscation(isPreview: boolean): Promise<void> {
             loadingPreview.value = true;
 
             //Fetch a preview of the obfuscation
-            let dataset = (await $fetch('/api/anonymizer/preview', {
+            const response = await useFetch('/api/anonymizer/preview', {
                 method: 'POST',
                 body: {
                     config: obfuscationBody,
                 },
-            })) as Dataset;
+            });
 
-            console.log('API Response', dataset)
-            obfuscatedRows.value = formatPreview(dataset);
+            console.log('Full API Response:', response);
+
+            obfuscatedRows.value = formatPreview(response.result.dataset);
+            const metadata = response.result.metadata as any;
+            riskMetrics.value = metadata.risk;
 
             riskMetrics.value = dataset.result.metadata.risk;
 
