4a4cb28d3999bd99e2a781520580cb9766b642bc
Ahmad Al Asadi <45769428+A7mat@users.noreply.github.com>
2026-06-08 12:33:19 +0200

    Pistis access control (#479)
    auth token to search request

--- pages/catalog/useDatasetsSearchView.ts
+++ pages/catalog/useDatasetsSearchView.ts
@@ -63,6 +63,10 @@ export function useDatasetSearchView<TF extends string, TM, TS extends EnhancedS
     //         ? { Authorization: `Bearer ${searchQueryArgs.forceUserToken ?? authStore.user.token}` }
     //         : {}),
     // }));
+    const { data: session } = useAuth();
+    const searchHeaders = computed(() =>
+        session.value?.token ? { Authorization: `Bearer ${session.value.token}` } : {},
+    );
 
     // --- Dataset Scope: Model Toggles & Computed Flags ---
     const hvdModel = toRef(options?.hvdModel || false);
@@ -93,7 +97,7 @@ export function useDatasetSearchView<TF extends string, TM, TS extends EnhancedS
             ...toRefs(selectedFacets.value),
             periodicity: isLivedata,
         },
-        // headers: searchHeaders,
+        headers: searchHeaders,
     });
 
     const searchMetaData = computed(() => {
